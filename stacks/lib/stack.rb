fae8ea1e79047afdebbda63420c592d52e5bb988
fishermanswharff <fishermanswharff@github.com>
2015-03-30 09:06:58 -0400

    stack spec

--- stacks/lib/stack.rb
+++ stacks/lib/stack.rb
@@ -40,5 +40,3 @@ class Stack
   end
 
 end
-
-binding.pry
--- stacks/spec/stack_spec.rb
+++ stacks/spec/stack_spec.rb
@@ -1,2 +1,26 @@
 require 'spec_helper'
 require_relative '../lib/stack'
+
+describe Stack do
+
+  before(:all) do
+    @stack = Stack.new(1)
+  end
+
+  it 'initializes successfully' do
+    expect(@stack.class).to eq(Stack)
+    expect(@stack.head.value).to eq(1)
+  end
+
+  it 'adds to the stack' do
+    @stack.push(2)
+    expect(@stack.head.value).to eq(2)
+    expect(@stack.head.next_node.value).to eq(1)
+  end
+
+  it 'removes the first item from the stack' do
+    @stack.push(2)
+    @stack.pop
+    expect(@stack.head.value).to eq(2)
+  end
+end
