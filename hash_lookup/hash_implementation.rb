c1198d6af32eeaa0ff0809242e39d731b36a90da
fishermanswharff <fishermanswharff@github.com>
2015-03-30 10:40:46 -0400

    adding back in hash_implementation

--- hash_lookup/hash_implementation.rb
+++ hash_lookup/hash_implementation.rb
@@ -3,7 +3,30 @@ require 'pry'
 HashEntry = Struct.new(:key, :value)
 
 class HashTable
-  # your code goes here
+  attr_accessor :bins
+  attr_accessor :bin_count
+
+  def initialize
+    self.bins = []
+    self.bin_count = 11
+  end
+
+  def bin_for(key)
+    key.hash % self.bin_count
+  end
+
+  def <<(entry)
+    index = bin_for(entry.key)
+    self.bins[index] ||= []
+    self.bins[index] << entry
+  end
+
+  def [](key)
+    index = bin_for(key)
+    self.bins[index].detect do |entry|
+      entry.key == key
+    end
+  end
 end
 
 entry = HashEntry.new :foo, :bar
